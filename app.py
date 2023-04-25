86187e0c1981ed397e594f3b96c32a869e00978b
xypage <xypage@gmail.com>
2023-04-25 15:49:05 -0700

    Hooked existing front end up to basic flask app

---
+++ app.py
@@ -0,0 +1,45 @@
+###################################################
+#                     Imports                     #
+###################################################
+# Generic imports
+from flask import Flask, render_template, request, url_for, redirect, session
+
+# For the session secret key
+from os import urandom
+
+app = Flask(__name__)
+# Set a secret_key to use session/login_manager
+app.secret_key = urandom(12)
+
+##################################################
+#               Debugging functions              #
+##################################################
+
+from sys import stdout
+# Since print needs to have the file specified to stdout just make a separate function to simplify debug calls
+def p(*kwargs):
+    print(kwargs, file=stdout)
+
+
+
+###################################################
+#                Route definitions                #
+###################################################
+# Just to avoid the favicon error, favicon.ico is an empty file
+@app.route("/favicon.ico")
+def favicon():
+    return redirect(url_for("static", filename="favicon.ico"))
+
+
+@app.route("/")
+def home():
+    return redirect(url_for("off"))
+
+@app.route("/off")
+def off():
+    return render_template("off.html")
+    
+@app.route("/on")
+def on():
+    return render_template("on.html")
+    
