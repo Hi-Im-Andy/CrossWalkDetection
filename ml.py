83df1a7ef0e84a82c417150d97bed949087dbad4
Andy <60999770+Hi-Im-Andy@users.noreply.github.com>
2023-04-26 05:31:13 -0700

    Small changes

--- ml.py
+++ ml.py
@@ -4,8 +4,8 @@ from tts import say
 ped_model = YOLO('YOLOpedlight/yolov8l_custom.pt')
 # cross_model = YOLO('YOLOcrosswalk/yolov8l_custom.pt') #Uncomment this for crosswalk
 
-ped_results = ped_model('YOLOpedlight/val/images/image71.jpg')
-print(ped_results)
+ped_result = ped_model('YOLOpedlight/val/images/image71.jpg')
+print(ped_result)
 
 
 # Results is a list, need to extract just the predicted value 1 (go) or 0 (stop)
@@ -16,19 +16,22 @@ print(ped_results)
 def check_pedlight(live_feed):
     ped_result = ped_model(live_feed) # Need to find location of the live feed
     
-    if(ped_results == 1): # If sign shows go
+    if(ped_result == 1): # If sign shows go
         say("You can cross")
     
-    else:
+    elif(ped_result == 0):
         say("Wait")
+
+    else:
+        say("Adjust camera to locate sign")
 
 
 # Same as the previous code but for the crosswalk instead of the light
-# def check_crosswalk(live_feed):
-#     cross_result = cross_model(live_feed)
+def check_crosswalk(live_feed):
+    cross_result = cross_model(live_feed)
 
-#     if(cross_results == 1):
-#         say("This is a crosswalk.")
-#     else:
-#         say("No crosswalk detected.")
+    if(cross_result == 1):
+        say("This is a crosswalk.")
+    else:
+        say("No crosswalk detected.")
 
